45663fd5c888e32a524e57f5165e239edd37193a
asus <asus@DESKTOP-BNNR7CL>
2017-12-03 02:59:01 -0200

    código inicial

--- SistemaFrequencia/src/model/Funcionario.java
+++ SistemaFrequencia/src/model/Funcionario.java
@@ -7,4 +7,23 @@ package model;
  */
 public class Funcionario{
     
+    String nome;
+    String cpf;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    
 }
--- SistemaFrequencia/src/model/Gerente.java
+++ SistemaFrequencia/src/model/Gerente.java
@@ -1,10 +1,14 @@
 
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author asus
  */
 public class Gerente extends Funcionario{
+    
+    ArrayList <Funcionario> funcSupervisionados;
     
 }
